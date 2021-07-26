6463ef348345106aa8cd1fcbb80e94ff1b9507ba
unknown <ahmed.e.elkharadly@gmail.com>
2021-07-27 01:58:59 +0200

    'ok'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import { HomePage } from "./homepage-comp";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
